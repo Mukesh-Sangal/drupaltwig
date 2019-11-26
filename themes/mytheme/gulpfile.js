3ec1e070b2c334326e07b1076a8220a2a17f5202
Mukesh-Sangal <mukeshsangal94@gmail.com>
2019-11-26 11:14:10 +0530

    first commit

---
+++ themes/mytheme/gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var livereload = require('gulp-livereload')
+var uglify = require('gulp-uglifyjs');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+sass.compiler = require('node-sass');
+
+gulp.task('imagemin', function () {
+    return gulp.src('./themes/custom/mytheme/images/*') 
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./themes/custom/mytheme/images'));
+});
+gulp.task('sass', function () {
+    return gulp.src('sass/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+gulp.task('uglify', function() {
+  gulp.src('./themes/custom/mytheme/lib/*.js')
+    .pipe(uglify('main.js'))
+    .pipe(gulp.dest('./themes/custom/mytheme/js'))
+});
+gulp.task('watch', function(){
+    livereload.listen();
+    gulp.watch('sass/**/*.scss', gulp.series('sass'));
+    gulp.watch('./lib/*.js', gulp.series('uglify'));
+    gulp.watch(['/css/style.css', './**/*.twig', './js/*.js'], function (files){
+        livereload.changed(files)
+    });
+});
